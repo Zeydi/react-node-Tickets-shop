655b12c54cfa2d1eacef5fcb5623fdc9117c12b4
Zeydi <zeidiwassa@gmail.com>
2023-07-25 10:40:24 +0200

    added db connection for the auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
@@ -9,8 +10,20 @@ const app = express();
 app.use(json());
 
 app.use(signupRouter);
+
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to db");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!");
+  });
+};
+
+start();

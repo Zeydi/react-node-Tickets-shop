d7944866b82506077eca0ad40fdad0c6d38f5dd2
Zeydi <zeidiwassa@gmail.com>
2023-07-25 09:47:00 +0200

    added error handling middleware and error types

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+
 const app = express();
 app.use(json());
 
-
 app.use(signupRouter);
+app.use(errorHandler);
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000!!!');
+  console.log("Listening on port 3000!!!");
 });
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,27 +1,28 @@
-import express, {Request, Response} from 'express';
-import { body, validationResult } from 'express-validator';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-errors";
+import { DatabaseConnectionError } from "../errors/database-connection-errors";
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-    body('email')
-        .isEmail()
-        .withMessage('Email in Invalid'),
-    body('password')
-        .trim()
-        .isLength({ min: 4, max: 20 })
-        .withMessage('Password must be between 4 and 20 characters')    
-    ], 
-    (req: Request, res: Response) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).send(errors.array());
-        }    
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email is Invalid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
 
-
-        const { email, password } = req.body;
-        console.log('Creating a user ...');
-        res.send({});
-});
+    console.log("Creating a user ...");
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
